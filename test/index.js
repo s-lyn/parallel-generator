fc1e35b31f74b967dab06e008d7b69e969473cf9
Serhii Lynnyk <serhii.lynnyk@gmail.com>
2019-07-23 16:27:58 +0300

    Add async test

--- test/index.js
+++ test/index.js
@@ -9,10 +9,26 @@ describe('parallel()', function () {
     assert.strictEqual(next.value, undefined)
     assert.strictEqual(next.done, true)
   })
-  it.only('should exec sync tasks', async function () {
+  it('should exec sync tasks', async function () {
     const tasks = [
       call(() => 'One'),
       call(() => 'Two')
+    ]
+    const iterator = parallel(tasks)
+    let next = await iterator.next()
+    assert.strictEqual(next.value, 'One')
+    assert.strictEqual(next.done, false)
+    next = await iterator.next()
+    assert.strictEqual(next.value, 'Two')
+    assert.strictEqual(next.done, false)
+    next = await iterator.next()
+    assert.strictEqual(next.value, undefined)
+    assert.strictEqual(next.done, true)
+  })
+  it('should exec async tasks', async function () {
+    const tasks = [
+      call(() => Promise.resolve('One')),
+      call(() => Promise.resolve('Two'))
     ]
     const iterator = parallel(tasks)
     let next = await iterator.next()
